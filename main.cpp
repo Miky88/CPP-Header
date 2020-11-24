a938eb8cb8f47ff899146f1dbba3676e6d314fb9
Miky88 <44903677+Miky88@users.noreply.github.com>
2020-11-24 21:01:49 +0100

    v2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
+#include <iterator>
 using namespace std;
-int main () {
-	const int R = 10;
-	const int C = 40;
-	string TEXT[] = {"Hello World!", "Hello everybody!", "Hello World! Hello World! Hello World!"};
-	int size = sizeof(TEXT)/sizeof(TEXT[0]);
 
+int header(string array[], int size, int columns = 40) {
 	cout << char (201);
-	for (int i=1; i<=C; i++)
-		cout << char (205);
+	for (int i=1; i<=columns; i++) cout << char (205);
 	cout << char (187) << endl;
-	
 	for (int i=0; i<size; i++) {
 		cout << char (186);
-		for (int j=0; j <=(C-2)/2-TEXT[i].length()/2; j++)
-			cout << char(32);
-		cout << TEXT[i];
-		for (int j=0; j <=(C-2)/2-TEXT[i].length()/2; j++)
-			cout << char(32);
+		for (int j=0; j <=(columns-2)/2-array[i].length()/2; j++) cout << char(32);
+		cout << array[i];
+		for (int j=0; j <=(columns-2)/2-array[i].length()/2; j++) cout << char(32);
 		cout << char (186) << endl;
 	}
-		
 	cout << char (200);
-	for (int j=1; j<=C; j++)
-		cout << char (205);
+	for (int j=1; j<=columns; j++) cout << char (205);
 	cout << char (188) << endl;
+	return 0;
+}
+
+int main () {
+	string head[] = {
+		"Hello World!",
+		"Hello everybody!",
+		"Hello World! Hello World! Hello World!"
+	};
+	
+	header(head, 3);
 }
